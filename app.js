25444642b356ef5bd699be6d060782294d0a6f74
cDraganTRfc <dragantrifunovic@yahoo.com>
2023-10-11 15:39:16 +0200

    add style to  main section

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
 const form = document.querySelector("form");
 
 
-const searchWord = (e) => {
+const searchWord = async (e) => {
     e.preventDefault();
 
+    const data = await getData();
+    console.log(data)
+};
+
+const renderHtml = (data) => {
+    let html = "";
+    html+=`<div class="word-header">
+    <div class="left">
+        <h1>${}</h1>
+        <p>/'ki:bc:d/</p>
+    </div>
+    <div class="right">
+        <button>
+            <audio src=""></audio>
+        </button>
+    </div>
+</div>`
 }
 
 const getData = async () => {
-    const response = await fetch ('https://api.dictionaryapi.dev/api/v2/entries/en/hello')
+    const response = await fetch ('https://api.dictionaryapi.dev/api/v2/entries/en/keyboard')
 
 const data = await response.json();
-console.log(data)
+
 };
 
 form.addEventListener("submit", searchWord); 
